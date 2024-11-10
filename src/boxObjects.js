d4ca6dfbb19b98e0dbdcea31376719410695835b
Monopavo <monopavomedia@gmail.com>
2024-11-10 04:37:25 +0000

    Update boxObjects.js: Add all icons.

--- src/boxObjects.js
+++ src/boxObjects.js
@@ -4,9 +4,32 @@ import moba from "./assets/moba.svg";
 import racing from "./assets/racing.svg";
 import sports from "./assets/sports.svg";
 import social from "./assets/social.svg";
-import sandbox from "./assets/social.svg";
+import sandbox from "./assets/sandbox.svg";
+import openWorld from "./assets/open-world.svg";
+import survival from "./assets/survival.svg";
+import zombie from "./assets/zombie.svg";
+import turnBased from "./assets/turn-based.svg";
+import tank from "./assets/tank.svg";
+import space from "./assets/space.svg";
+import sailing from "./assets/sailing.svg";
+import sideScroller from "./assets/side-scroller.svg";
+import superhero from "./assets/superhero.svg";
+import permadeath from "./assets/permadeath.svg";
+import card from "./assets/card.svg";
+import battleRoyale from "./assets/battle-royale.svg";
+import mmo from "./assets/mmo.svg";
+import fantasy from "./assets/fantasy.svg";
+import sciFi from "./assets/sci-fi.svg";
+import fighting from "./assets/fighting.svg";
+import military from "./assets/military.svg";
+import martialArts from "./assets/martial-arts.svg";
+import flight from "./assets/flight.svg";
+import towerDefense from "./assets/tower-defense.svg";
+import horror from "./assets/horror.svg";
 
 const boxObjects = {
+
+    /* "mmorpg": { icon: , description: ""}, */
     "shooter": { icon: shooter, description:""},
     "strategy": { icon: strategy, description: ""},
     "moba": { icon: moba, description: ""},
@@ -14,43 +37,43 @@ const boxObjects = {
     "sports": { icon: sports, description: ""},
     "social": { icon: social, description: ""},
     "sandbox": { icon: sandbox, description: ""},
-    "open-world": { description: ""},
-    "survival": { description: ""},
-    "pvp": { description: ""},
-    "pve": { description: ""},
-    "pixel": { description: ""},
-    "voxel": { description: ""},
-    "zombie": { description: ""},
-    "turn-based": { description: ""},
-    "first-person": { description: ""},
-    "third-Person": { description: ""},
-    "top-down": { description: ""},
-    "tank": { description: ""},
-    "space": { description: ""},
-    "sailing": { description: ""},
-    "side-scroller": { description: ""},
-    "superhero": { description: ""},
-    "permadeath": { description: ""},
-    "card": { description: ""},
-    "battle-royale": { description: ""},
-    "mmo": { description: ""},
-    "mmofps": { description: ""},
-    "mmotps": { description: ""},
-    "3d": { description: ""},
-    "2d": { description: ""},
-    "anime": { description: ""},
-    "fantasy": { description: ""},
-    "sci-fi": { description: ""},
-    "fighting": { description: ""},
-    "action-rpg": { description: ""},
-    "action": { description: ""},
-    "military": { description: ""},
-    "martial-arts": { description: ""},
-    "flight": { description: ""},
-    "low-spec": { description: ""},
-    "tower-defense": { description: ""},
-    "horror": { description: ""},
-    "mmorts": { description: ""}
+    "open-world": { icon: openWorld, description: ""},
+    "survival": { icon: survival, description: ""},
+    /* "pvp": { icon: , description: ""}, */
+    /* "pve": { icon: , description: ""}, */
+    /* "pixel": { icon: , description: ""}, */
+    /* "voxel": { icon: , description: ""}, */
+    "zombie": { icon: zombie, description: ""},
+    "turn-based": { icon: turnBased, description: ""},
+    /* "first-person": { icon: , description: ""}, */
+    /* "third-Person": { icon: , description: ""}, */
+    /* "top-down": { icon: , description: ""}, */
+    "tank": { icon: tank, description: ""},
+    "space": { icon: space, description: ""},
+    "sailing": { icon: sailing, description: ""},
+    "side-scroller": { icon: sideScroller, description: ""},
+    "superhero": { icon: superhero, description: ""},
+    "permadeath": { icon: permadeath, description: ""},
+    "card": { icon: card, description: ""},
+    "battle-royale": { icon: battleRoyale, description: ""},
+    "mmo": { icon: mmo, description: ""},
+    /* "mmofps": { icon: , description: ""}, */
+    /* "mmotps": { icon: , description: ""}, */
+    /* "3d": { icon: , description: ""}, */
+    /* "2d": { icon: , description: ""}, */
+    /* "anime": { icon: , description: ""}, */
+    "fantasy": { icon: fantasy, description: ""},
+    "sci-fi": { icon: sciFi, description: ""},
+    "fighting": { icon: fighting, description: ""},
+    /* "action-rpg": { description: ""}, */
+    /* "action": { description: ""}, */
+    "military": { icon: military, description: ""},
+    "martial-arts": { icon: martialArts, description: ""},
+    "flight": { icon: flight, description: ""},
+    /* "low-spec": { icon: , description: ""}, */
+    "tower-defense": { icon: towerDefense, description: ""},
+    "horror": { icon: horror, description: ""},
+    /* "mmorts": { icon: , description: ""}, */
 };
 
 export default boxObjects;
