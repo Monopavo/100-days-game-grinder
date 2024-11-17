03aaaf784596ec44ac56edde4fcea3ba2b8cd6f7
Monopavo <monopavomedia@gmail.com>
2024-11-17 13:27:48 +0000

    Prepare deploy.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repoName = '100-days-game-grinder';
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  
+
   return {
     plugins: [react()],
     define: {
       'process.env': env
+    },
+    base: `/${repoName}/`,
+    build: {
+      outDir: 'dist'
     }
   };
 });
